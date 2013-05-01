1278628b6018d659f526330a5089d3034b945d00
arturo <arturo@openframeworks.cc>
2013-05-01 12:58:40 +0200

    docs: collapse all categories button

--- js/documentation_index.js
+++ js/documentation_index.js
@@ -1,6 +1,9 @@
 var currentHidding=null;
 var coreFunctionsHidden=false;
 var addonsFunctionsHidden=false;
+var coreCollapsed=false;
+var addonsCollapsed = false;
+
 $(function(){
     $('.documentation_index_group').masonry({
         itemSelector: '.box',
@@ -12,27 +15,28 @@ $(function(){
 
 $(document).ready(
     function(){
+        // collapse one
         $('div.documentation_group_head').click(function () {
         if(currentHidding!=null) return;
         currentHidding = $(this);
             if($(this).hasClass('show')){
-                $(this).next('div.documentation_index_group').hide("blind", { direction: "vertical" }, 500, function(){
-                    currentHidding.children('h2').first().text("+"+currentHidding.children('h2').first().text().substring(1));
+                $(this).next('.documentation_index_group').hide("blind", { direction: "vertical" }, 500, function(){
                     currentHidding.removeClass('show');
                     currentHidding.addClass('hide');
                     currentHidding = null;
                     });
             }else{
-                $(this).next('div.documentation_index_group').show("blind", { direction: "vertical" }, 500, function(){
-                    currentHidding.children('h2').first().text("-"+currentHidding.children('h2').first().text().substring(1));
+                $(this).next('.documentation_index_group').show("blind", { direction: "vertical" }, 500, function(){
                     currentHidding.removeClass('hide');
                     currentHidding.addClass('show');
                     currentHidding = null;
-                    $('.documentation_index_group').masonry( 'reload' );
+                    $(this).masonry( 'reload' );
                     });
             }
         });
         
+        
+        // hide show functions
         $('a.hide_core_functions').click(function(){
             if(coreFunctionsHidden){
                 $('.core ul.documentation_class li.method').show();
@@ -41,9 +45,9 @@ $(document).ready(
                 $(this).text('hide functions');
             }else{
                 $('.core ul.documentation_class li.method').hide();
-                $('.core .documentation_index_group').masonry( 'reload' );
                 coreFunctionsHidden = true;
                 $(this).text('show functions');
+                $('.core .documentation_index_group').masonry( 'reload' );
             }
         });
         $('a.hide_addons_functions').click(function(){
@@ -54,9 +58,43 @@ $(document).ready(
                 $(this).text('hide functions');
             }else{
                 $('.addons ul.documentation_class li.method').hide();
-                $('.addons .documentation_index_group').masonry( 'reload' );
                 addonsFunctionsHidden = true;
                 $(this).text('show functions');
+                $('.addons .documentation_index_group').masonry( 'reload' );
+            }
+        });
+        
+        // collapse all
+        $('a.collapse_core').click(function(){
+            if(coreCollapsed){
+                $('.core .documentation_index_group').show();
+                $('.core .documentation_index_group').masonry( 'reload' );
+                $('.core .documentation_group_head').addClass('show');
+                $('.core .documentation_group_head').removeClass('hide');
+                coreCollapsed = false;
+                $(this).text('collapse all');
+            }else{
+                $('.core div.documentation_index_group').hide();
+                $('.core .documentation_group_head').addClass('hide');
+                $('.core .documentation_group_head').removeClass('show');
+                coreCollapsed = true;
+                $(this).text('open all');
+            }
+        });
+        $('a.collapse_addons').click(function(){
+            if(addonsCollapsed){
+                $('.addons .documentation_index_group').show();
+                $('.addons .documentation_index_group').masonry( 'reload' );
+                $('.addons .documentation_group_head').addClass('show');
+                $('.addons .documentation_group_head').removeClass('hide');
+                addonsCollapsed = false;
+                $(this).text('collapse all');
+            }else{
+                $('.addons .documentation_index_group').hide();
+                $('.addons .documentation_group_head').addClass('hide');
+                $('.addons .documentation_group_head').removeClass('show');
+                addonsCollapsed = true;
+                $(this).text('open all');
             }
         });
 });
