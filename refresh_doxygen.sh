82fc02afe252e470c17b869c5c3f1584d4301ab6
arturo <arturo@openframeworks.cc>
2014-04-03 22:06:59 +0200

    fix refresh_doxygen

--- refresh_doxygen.sh
+++ refresh_doxygen.sh
@@ -2,11 +2,11 @@
 OF_ROOT=/home/arturo/Desktop/openFrameworks
 OF_SITE_ROOT=$(pwd)
 
-cd $OF_ROOT/libs/openFrameworksCompiled/project/doxygen && doxygen
+cd $OF_ROOT/libs/openFrameworksCompiled/project/doxygen && doxygen Doxyfile
 cd $OF_SITE_ROOT
 mkdir doxygen
 rm -r doxygen/* 
-cp -R "$OF_ROOT/libs/openFrameworksCompiled/project/doxygen/build/html/" doxygen/ 
+cp -R $OF_ROOT/libs/openFrameworksCompiled/project/doxygen/build/html/* doxygen/ 
 cd doxygen/
 for f in *.html; do mv $f $f.mako; done
 cd ..
