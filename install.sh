5bfa5cd75ee610c48f3dba2db81823d3f0f4d904
arturoc <arturo@openframeworks.cc>
2016-01-21 15:17:40 +0000

    install python3 and pip3

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
-pip install virtualenv
+OS=$(uname -s)
+PIP=pip
+if [ "$OS" == "Linux" ]; then
+	sudo apt-get install python3 python3-pip
+	PIP=$(ls /usr/bin/pip-3* | head -n1)
+fi
+if [ "$(which python3)" == "" ]; then
+	echo ofSite needs python3 to run, please install it before running this script
+	exit 1
+fi
+sudo $PIP install virtualenv
 virtualenv -p $(which python3) nikola
 source nikola/bin/activate
-pip install --upgrade "Nikola[extras]"
-pip install --upgrade python-Levenshtein
+$PIP install --upgrade "Nikola[extras]"
+$PIP install --upgrade python-Levenshtein
 
