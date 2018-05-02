825a2275435eef8c78bb423b013c2c0b1334f2a3
arturo castro <arturo@openframeworks.cc>
2018-05-02 12:46:19 +0200

    build_docs.sh: fix checkout + add bash env

--- build_docs.sh
+++ build_docs.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 rm -r documentation
-git checkout _documentation
+git checkout documentation
 source nikola/bin/activate
 LD_LIBRARY_PATH=/usr/lib/llvm-6.0/lib/ plugins/documentation/documentation/tools/documentation_update.py
 if [ "$?" == "0" ]; then
