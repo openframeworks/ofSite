a540918d8cea92f5e39ba5d20951771b09861b9f
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-15 17:31:57 +0800

    Delete themes/openframeworks directory

--- themes/openframeworks/assets/js/development.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('#core_contributors').columnize({ columns: 3 });
-    $('#site_contributors').columnize({ columns: 3 });
-    $('#book_contributors').columnize({ columns: 3 });
-    $('#addons_contributors').columnize({ columns: 3 });
-    $('#apothecary_contributors').columnize({ columns: 3 });
-    $('#pg_contributors').columnize({ columns: 3 });
-});
