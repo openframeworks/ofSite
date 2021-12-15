9c466548706c9a342e29bc64fba20f1866a3e6f6
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-15 18:03:39 +0800

    Delete build_docs.sh

--- build_docs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-rm -r documentation
-git checkout documentation
-source nikola/bin/activate
-LD_LIBRARY_PATH=/usr/lib/llvm-6.0/lib/ plugins/documentation/documentation/tools/documentation_update.py
-if [ "$?" == "0" ]; then
-	nikola build
-fi
-deactivate
